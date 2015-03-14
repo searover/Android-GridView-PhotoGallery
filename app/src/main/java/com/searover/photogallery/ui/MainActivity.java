359efbc6ba980bbe899b021820dc95cb1afb5bd0
Baiguang <alex987965@gmail.com>
2015-03-14 19:00:05 +0800

    create packages

--- app/src/main/java/com/searover/photogallery/ui/MainActivity.java
+++ app/src/main/java/com/searover/photogallery/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.searover.photogallery;
+package com.searover.photogallery.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.searover.photogallery.R;
 
 
 public class MainActivity extends ActionBarActivity {
