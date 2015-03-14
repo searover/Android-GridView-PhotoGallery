ba7441aa6b2e278d1e323e2196652c71f4d291fc
Baiguang <alex987965@gmail.com>
2015-03-14 19:12:16 +0800

    add gridview styles add photo resources

---
+++ app/src/main/java/com/searover/photogallery/provider/Photos.java
@@ -0,0 +1,46 @@
+package com.searover.photogallery.provider;
+
+/**
+ * Created by searover on 3/14/15.
+ */
+public class Photos {
+    public final static String[] imageThumbUrls = new String[]{
+            "http://img0.bdstatic.com/img/image/%E6%9C%AA%E6%A0%87%E9%A2%98-1.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/lyngaomen.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/lygly.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/lylbjzg.jpg",
+            "http://img0.bdstatic.com/img/image/1%E8%A5%BF%E6%B8%B8%E8%AE%B0%E2%80%94%E2%80%94%E8%A5%BF%E7%8F%AD%E7%89%99.jpg",
+            "http://img0.bdstatic.com/img/image/%E6%84%8F%E5%A4%A7%E5%88%A9%E2%80%94%E2%80%94%E5%8F%AA%E4%B8%80%E7%9C%BC%EF%BC%8C%E4%BE%BF%E6%B0%B8%E8%BF%9C.jpg",
+            "http://img0.bdstatic.com/img/image/beihaidao180180.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%B7%B4%E8%A5%BF.jpg",
+            "http://img0.bdstatic.com/img/image/riben180180.jpg",
+            "http://img0.bdstatic.com/img/image/%E7%91%9E%E5%A3%AB-%E5%BE%B7%E5%9B%BD%E8%87%AA%E7%94%B1%E8%A1%8C.jpg",
+            "http://img0.bdstatic.com/img/image/xizang180180.jpg",
+            "http://img0.bdstatic.com/img/image/%E7%BE%8E%E5%9B%BD%E5%90%8D%E6%A0%A1.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%89%91%E6%A1%A5%E5%9B%BE%E4%B9%A6%E9%A6%86.jpg",
+            "http://img0.bdstatic.com/img/image/%E6%97%A5%E6%9C%AC%E5%A4%A7%E5%AD%A6.jpg",
+            "http://img0.bdstatic.com/img/image/%E7%BE%8E%E5%9B%BD%E5%A4%A7%E5%AD%A6.jpg",
+            "http://img0.bdstatic.com/img/image/%E8%8B%B1%E5%9B%BD%E5%90%8D%E6%A0%A1%E5%9B%BE%E4%B9%A6%E9%A6%86.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%A4%A7%E9%98%AA%E5%A4%A7%E5%AD%A6.jpg",
+            "http://img0.bdstatic.com/img/image/%E8%8B%B1%E5%9B%BD%E5%9F%8E%E5%A0%A1%E5%A4%A7%E5%AD%A6.jpg",
+            "http://img0.bdstatic.com/img/image/%E6%AC%A7%E6%B4%B2%E5%A4%A7%E5%AD%A6.jpg",
+            "http://img0.bdstatic.com/img/image/%E9%BB%84%E7%9F%B3%E7%AB%96%E5%9B%BE.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/djrmsllk1.jpg",
+            "http://img0.bdstatic.com/img/image/%E6%99%AE%E5%90%89%E5%B2%9B%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/djrmwns.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/djrmlj.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%A4%8F%E5%A8%81%E5%A4%B7%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%B7%B4%E5%8E%98%E5%B2%9B%E6%A8%AA.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%8C%97%E6%B5%B7%E9%81%93%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/%E9%A9%AC%E5%B0%94%E4%BB%A3%E5%A4%AB%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/guoneixizang.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/guoneixianggelila.jpg",
+            "http://img0.bdstatic.com/img/image/shouye/guoneiguilin.jpg",
+            "http://img0.bdstatic.com/img/image/%E4%B8%89%E4%BA%9A%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/%E4%BA%91%E5%8D%97%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/%E5%B9%BF%E8%A5%BF%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/%E4%B9%9D%E5%AF%A8%E6%B2%9F%E6%A8%AA.jpg",
+            "http://img0.bdstatic.com/img/image/%E4%B8%8A%E6%B5%B7%E6%96%B9.jpg",
+            "http://img0.bdstatic.com/img/image/%E8%8B%8F%E5%B7%9E%E6%96%B9.jpg",
+    };
+}
